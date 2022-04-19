8e135fd10299724c5b51cb136a5008c258791cb1
Acuah <danielacquah579@gmaile.com>
2022-04-19 15:00:08 +0000

    m,mf

---
+++ script.js
@@ -0,0 +1,19 @@
+var xq = 30;
+var ys = 15;
+
+console.log( xq + ys );
+
+let add = 10;
+let time = 5;
+console.log(add * time );
+
+var myage = 10;
+var mysisage = 30;
+var u = 50;
+
+if (myage + mysisage >= u ) {
+    console.log('Good');
+}
+else {
+    console.log('Not good');
+}
